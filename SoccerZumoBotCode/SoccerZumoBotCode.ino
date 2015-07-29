7ee01dfabc56eb5fd9595f3af147e9c7b4c19ac2
Jake Wood <jake.wood@my.jcu.edu.au>
2015-07-29 23:44:35 +1000

    Started Creating the soccer code for my sumo bot

---
+++ SoccerZumoBotCode/SoccerZumoBotCode.ino
@@ -0,0 +1,18 @@
+
+int throttleValue = 100;
+int steeringValue = 100;
+int disconnectCounter = 0;
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
